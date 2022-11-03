93e3ea3312c1fb997b0d09e6fe42acd122310925
nachle <elias.nachle@bandtec.com.br>
2022-11-02 23:54:44 -0300

    feat: adicionando animacao

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,60 @@
-import * as Style from './Home.style'
-import Navbar from '../components/navbar/Navbar'
-import logoBarber from '../assets/kenny-barbeiro.png'
-import RedirectList from '../components/redirectList/RedirectList'
+import * as Style from './Home.style';
+import Navbar from '../components/navbar/Navbar';
+import logoBarber from '../assets/kenny-barbeiro.png';
+import RedirectList from '../components/redirectList/RedirectList';
+import { motion } from 'framer-motion';
 
 export default function Home() {
+  const backgroundAnimation = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: { ease: 'easeIn', duration: 1.5 },
+    },
+    exit: { opacity: 1 },
+  };
+
+  const cardBarberAnimation = {
+    hidden: { opacity: 0, y: 15 },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: { ease: 'easeInOut', duration: 1.5, delay: 0.45 },
+    },
+    exit: { opacity: 1 },
+  };
+
   return (
     <>
       <Style.Container>
         <Style.ContainerInfo>
           <Navbar />
-          <Style.CardBarber>
-            <img src={logoBarber} alt='Kenny Barbeiro' />
-            <RedirectList />
-          </Style.CardBarber>
+          <motion.div
+            variants={cardBarberAnimation}
+            initial="hidden"
+            animate="show"
+            exit="exit"
+          >
+            <Style.CardBarber>
+              <motion.div>
+                <img src={logoBarber} alt="Kenny Barbeiro" />
+              </motion.div>
+              <RedirectList />
+            </Style.CardBarber>
+          </motion.div>
           <Style.Footer>
             <span>Kenny Barbeiro © 2023</span>
           </Style.Footer>
         </Style.ContainerInfo>
-        <Style.BarberBackground></Style.BarberBackground>
+        <motion.div
+          variants={backgroundAnimation}
+          initial="hidden"
+          animate="show"
+          exit="exit"
+        >
+          <Style.BarberBackground></Style.BarberBackground>
+        </motion.div>
       </Style.Container>
     </>
-  )
+  );
 }
