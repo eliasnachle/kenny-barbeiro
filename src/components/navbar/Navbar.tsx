4606289caffc94dfbf5c1176d4a7ecec278e435c
nachle <elias.nachle@bandtec.com.br>
2022-11-14 00:56:06 -0300

    refactor: limpando codigo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,17 +3,10 @@ import { Link } from 'react-router-dom';
 import { LanguageContext } from '../../context/LanguageContext';
 import { Languagens } from '../../languagens/LanguagemEnum';
 import * as Style from './Navbar.style';
-import { IoMdMenu, IoMdClose } from 'react-icons/io';
-/** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
 import LanguageSelector from './languageSelector/LanguageSelector';
 
 export default function Navbar() {
   const { setLanguage } = useContext(LanguageContext);
-  // const [isMobile, setMobile] = useState(false);
-  // const toggleMobileMenu = () => {
-  //   setMobile(!isMobile);
-  // };
 
   const handleLanguage = async (selectedLanguage: Languagens) => {
     setLanguage(selectedLanguage);
@@ -22,9 +15,6 @@ export default function Navbar() {
   return (
     <>
       <Style.Navbar>
-        {/* <Style.BurgerMenu>
-          <IoMdMenu onClick={toggleMobileMenu} />
-        </Style.BurgerMenu> */}
         <Style.LogoNavbar>
           <Link to="/">
             <span>Kenny Barbeiro</span>
@@ -33,45 +23,9 @@ export default function Navbar() {
         <ul>
           <li>
             <LanguageSelector />
-            {/* <Style.NavbarLink onClick={() => handleLanguage(Languagens.PT)}>
-              BR
-            </Style.NavbarLink>
-            <Style.NavbarLink onClick={() => handleLanguage(Languagens.EN)}>
-              EN
-            </Style.NavbarLink> */}
           </li>
         </ul>
       </Style.Navbar>
-
-      {/* <Style.MobileMenu
-        css={css`
-          left: ${isMobile ? '0%' : '-150%'};
-        `}
-      >
-        <div>
-          <IoMdClose onClick={toggleMobileMenu} />
-        </div>
-        <ul>
-          <li>
-            <Style.NavbarLink
-              onClick={() => {
-                handleLanguage(Languagens.PT);
-                toggleMobileMenu();
-              }}
-            >
-              BR
-            </Style.NavbarLink>
-            <Style.NavbarLink
-              onClick={() => {
-                handleLanguage(Languagens.EN);
-                toggleMobileMenu();
-              }}
-            >
-              EN
-            </Style.NavbarLink>
-          </li>
-        </ul>
-      </Style.MobileMenu> */}
     </>
   );
 }
